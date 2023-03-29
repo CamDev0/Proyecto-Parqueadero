9b1cbeb8cc0b8adde8e4c41d4d4843b092605dde
Camilo Barrios <camilobarriosdev@gmail.com>
2023-03-29 17:12:33 -0500

    Create some entities

---
+++ Parking.Shared/Entities/Parking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.Shared.Entities
+{
+    public class Parking
+    {
+        public int Id { get; set; }
+        public DateTime TimeParking { get; set; }
+        public Boolean Status { get; set; }
+    }
+}
